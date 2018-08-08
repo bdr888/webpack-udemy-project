972cc9d9f15fe07b7582053d7f2143aeb8878020
briandridge <briandridge@gmail.com>
2018-08-08 11:43:20 -0600

    feat(client): configure express server

--- webpack.config.js
+++ webpack.config.js
@@ -34,6 +34,9 @@ module.exports = {
     }),
     new HtmlWebpackPlugin({
       template: 'src/index.html'
+    }),
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
     })
   ]
 };
